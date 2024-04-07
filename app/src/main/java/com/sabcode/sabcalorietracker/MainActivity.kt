9080250fef723be5b680c40120da22173d207b4f
Sabuhi Amirov <amirovmirsabuhi@gmail.com>
2024-04-07 22:33:51 +0400

    Prepare welcomescreen and action button components

--- app/src/main/java/com/sabcode/sabcalorietracker/MainActivity.kt
+++ app/src/main/java/com/sabcode/sabcalorietracker/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.sabcode.onboarding_presentation.welcome.WelcomeScreen
 import com.sabcode.sabcalorietracker.ui.theme.SabCalorieTrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,30 +17,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SabCalorieTrackerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                   // color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                WelcomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SabCalorieTrackerTheme {
-        Greeting("Android")
     }
 }
