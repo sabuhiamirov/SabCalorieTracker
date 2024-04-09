7ce3aa37c1776a8ac81200fe68b7a09000c59014
Sabuhi Amirov <amirovmirsabuhi@gmail.com>
2024-04-09 12:12:10 +0400

    prepare preferences and add hilt

---
+++ core/src/main/java/com/sabcode/core/domain/preferences/Preferences.kt
@@ -0,0 +1,33 @@
+package com.sabcode.core.domain.preferences
+
+import com.sabcode.core.model.ActivityLevel
+import com.sabcode.core.model.Gender
+import com.sabcode.core.model.GoalType
+import com.sabcode.core.model.UserInfo
+
+interface Preferences {
+    fun saveGender(gender: Gender)
+    fun saveAge(age: Int)
+    fun saveWeight(weight: Float)
+    fun saveHeight(height: Int)
+    fun activityLevel(level: ActivityLevel)
+    fun saveGoalType(type: GoalType)
+    fun saveCarbRatio(ratio: Float)
+    fun saveProteinRatio(ratio: Float)
+    fun saveFatRatio(ratio: Float)
+
+    fun laodUserInfo() : UserInfo
+
+    companion object {
+        const val KEY_GENDER = "gemder"
+        const val KEY_AGE = "age"
+        const val KEY_WEIGHT = "weight"
+        const val KEY_HEIGHT = "height"
+        const val KEY_ACTIVITY_LEVEL = "level"
+        const val KEY_GOAL_TYPE = "goal_type"
+        const val KEY_CARB_RATIO = "carb_ratio"
+        const val KEY_PROTEIN_RATIO = "protein_ratio"
+        const val KEY_FAT_RATIO = "fat_ratio"
+    }
+
+}
