485b123a597ef48329d9eaef720450a6058d5a16
Sabuhi Amirov <amirovmirsabuhi@gmail.com>
2024-04-24 05:11:18 -0700

    Revert "create validate nutrient class and nutrient goal page"

--- onboarding/onboarding_presentation/src/main/java/com/sabcode/onboarding_presentation/nutrientGoal/NutrientGaolState.kt
+++
@@ -1,7 +0,0 @@
-package com.sabcode.onboarding_presentation.nutrientGoal
-
-data class NutrientGaolState(
-    val carbsRatio : String = "40",
-    val proteinRatio : String = "30",
-    val fatRatio : String = "30"
-)
--- onboarding/onboarding_presentation/src/main/java/com/sabcode/onboarding_presentation/nutrientGoal/NutrientGoalEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.sabcode.onboarding_presentation.nutrientGoal
-
-sealed class NutrientGoalEvent {
-    data class OnCarbRatioEnter(val ratio: String) : NutrientGoalEvent()
-    data class OnProteinRatioEnter(val ratio: String) : NutrientGoalEvent()
-    data class OnFatRatioEnter(val ratio: String) : NutrientGoalEvent()
-    object OnNextClick : NutrientGoalEvent()
-}
